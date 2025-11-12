568c7aa815237a4fae4fe6659c0256605e552f57
ali <alimurtazx@icloud.com>
2025-11-12 11:43:33 +0100

    Initial Commit

---
+++ DrawingApp/DrawingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingAppApp.swift
+//  DrawingApp
+//
+//  Created by ali on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DrawingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
